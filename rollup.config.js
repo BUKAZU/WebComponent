6cb64d921fc24acd7c5aa2651c43483a6901d00d
Bob van Oorschot <bobvanoorschot@gmail.com>
2023-01-31 13:48:13 +0100

    Use vite for development

--- rollup.config.js
+++ rollup.config.js
@@ -4,29 +4,42 @@ import json from '@rollup/plugin-json';
 import commonjs from '@rollup/plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
 import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
 
 export default {
   input: 'src/index.tsx',
+  external: ['react', 'react-dom'],
   output: [
     {
       file: 'build/index.js',
       format: 'cjs',
-      exports: 'auto',
+      exports: 'auto'
     },
+    {
+      file: 'build/index.min.js',
+      format: 'iife',
+      name: 'Portal',
+      globals: {
+        'react': 'react',
+        'react-dom': 'react-dom'
+      },
+      plugins: [terser()]
+    }
   ],
+  strictDeprecations: true,
   plugins: [
     typescript(),
     babel({
-      exclude: 'node_modules/**',
+      exclude: 'node_modules/**'
     }),
     postcss({
-      extract: true,
+      extract: true
     }),
     commonjs(),
     resolve(),
     json({
-      namedExports: false,
-    }),
+      namedExports: false
+    })
   ],
-  external: ['react', 'react-dom'],
+  external: ['react', 'react-dom']
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,27 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  plugins: [react()],
+  root: 'build',
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.tsx'),
+      name: 'BukazuPortal',
+      // the proper extensions will be added
+      formats: ['es', 'umd'],
+      fileName: (format) => `portal.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  }
+});
